99fee4d606e6278834469d7b2efc237e18b932a5
S. Costa <costastellam@gmail.com>
2020-11-27 01:03:45 -0300

    27/11/20

---
+++ ex.py
@@ -0,0 +1,13 @@
+'''valores = list()
+for cont in range (0,5):
+    valores.append(int(input("Digite um valor: ")))
+print(valores)
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Fim da lista')
+'''
+
+listA = [2,3,4,7]
+listB = listA
+print(listA)
+print(listB)
--- vogais.py
+++ vogais.py
@@ -1,7 +1,10 @@
 '''Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.'''
 
-palavra = str(input("Digite uma palavra: "))
+palavra = ('aprender', 'programar', 'python', 'faculdade', 'Cruzeiro')
 for p in palavra:
-    print(f'Na palavra {palavra} temos')
+    print(f'\nNa palavra {p.upper()} temos: ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=" ")
 
