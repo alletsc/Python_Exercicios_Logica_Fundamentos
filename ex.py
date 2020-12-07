7e2ac2162f5b58a2f0b8035d3d19260f1c459239
S. Costa <costastellam@gmail.com>
2020-12-07 12:28:07 -0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -7,7 +7,51 @@ for c, v in enumerate(valores):
 print('Fim da lista')
 '''
 
-listA = [2,3,4,7]
+'''listA = [2,3,4,7]
 listB = listA
 print(listA)
 print(listB)
+print(listA[0])'''
+
+'''pessoas = (['Pedro', 25], ['Ana', 36], ['Joao',78])
+print(pessoas[0][0])
+print(pessoas[1][1])
+print(pessoas[2][0])
+print(pessoas[1])'''
+
+'''teste = list()
+teste.append('Stella')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Josi'
+teste[1] = 23
+galera.append(teste[:])
+print(galera)'''
+
+'''galera = [['João', 25],['Ana', 34],['Stella', 23]]
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')'''
+
+
+'''
+galera = list()
+dado = list()
+totmai = totmen = 0
+for c in range(0,3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+
+for p in galera:
+    if p[1] >= 18:
+        print(f'{p[0]} é maior de idade.')
+        totmai += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmen +=1
+print(f'Temos {totmai} maiores e {totmen} menores de idade.')
+'''
+
+
