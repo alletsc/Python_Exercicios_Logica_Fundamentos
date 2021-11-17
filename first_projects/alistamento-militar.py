2d7a52db3ac9bea856b040cba27c41a42f40cbad
stella <stellacostam@gmail.com>
2021-11-17 13:54:22 -0300

    17/11

--- first_projects/alistamento-militar.py
+++ first_projects/alistamento-militar.py
@@ -3,6 +3,7 @@ de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar,
  se é a hora exata de se alistar ou se já passou do tempo do alistamento.
 Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.'''
 
+
 import datetime
 
 nascimento = int(input("Digite seu ano de nascimento: "))
